8dd3bacb42be9fc4be38c4e4b930545bd24c628a
Jj89757-bomb <1549728357@qq.com>
2026-03-26 10:38:08 +0800

    Update simple test.py

--- simple test.py
+++ simple test.py
@@ -1,4 +1,11 @@
 import streamlit as st
-
+from pymsis import msis
 st.title("Test succeed!")
-import pymsis
+import datetime
+
+time = datetime.datetime(2023, 1, 1, 12)
+lon = 0
+lat = 45
+alt = 400
+data=msis.run(time,lon,lat,alt)
+print(data)
